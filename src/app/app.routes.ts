11237b436bf56f77c0276d916f36a058da0d38bf
CalNielsen <cal.code.97@gmail.com>
2025-07-17 19:15:12 -0700

    Rough build-up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { UploadComponent } from './pages/upload/upload.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'upload', component: UploadComponent },
+    ],
+  },
+  { path: '**', redirectTo: '' },
+];
