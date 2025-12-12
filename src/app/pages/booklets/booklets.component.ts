7dc132bf66bff4a20f19c58e5e57d0deebfec38f
CalNielsen <cal.code.97@gmail.com>
2025-12-12 10:55:39 -0800

    booklets & catalogs

--- src/app/pages/booklets/booklets.component.ts
+++ src/app/pages/booklets/booklets.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FadeImageDirective } from '../../directives/fade-image.directive';
 import { SlideInDirective } from '../../directives/slide-in.directive';
+import { SeoService } from '../../services/seo.service';
 
 @Component({
   selector: 'app-booklets',
@@ -9,5 +10,10 @@ import { SlideInDirective } from '../../directives/slide-in.directive';
   styleUrl: './booklets.component.css'
 })
 export class BookletsComponent {
-
+  constructor(private seo: SeoService) {
+      this.seo.updatePageMeta({
+      title: 'Emergency Booklet Printing in San Diego, CA | Rush Orders',
+      description: 'Get high-quality custom booklets fast with Fresh Prints. We offer rush printing for catalogs, manuals, and marketing booklets in San Diego, CA.'
+    });
+    }
 }
--- src/app/pages/catalogs/catalogs.component.ts
+++ src/app/pages/catalogs/catalogs.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FadeImageDirective } from '../../directives/fade-image.directive';
 import { SlideInDirective } from '../../directives/slide-in.directive';
+import { SeoService } from '../../services/seo.service';
 
 @Component({
   selector: 'app-catalogs',
@@ -9,5 +10,11 @@ import { SlideInDirective } from '../../directives/slide-in.directive';
   styleUrl: './catalogs.component.css'
 })
 export class CatalogsComponent {
+  constructor(private seo: SeoService) {
+      this.seo.updatePageMeta({
+      title: 'Emergency Catalog Printing in San Diego, CA | Rush Order Printing',
+      description: 'Need catalogs printed fast in San Diego, CA? When deadlines are tight, we get your catalogs done on time. Call us now.'
+    });
+    }
 
 }
